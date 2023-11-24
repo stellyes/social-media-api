0f446945326ae2230ac9fb54bc293f7bf9c20438
Ryan England <me@rengland.org>
2023-11-24 14:24:26 -0800

    Added more base files

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const friendSchema = require("./Friend");
+const thoughtSchema = require("./Thought");
+const likeSchema = require("./Like");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            max_length: 32
+        },
+        email: {
+            type: String,
+            required: true,
+            min_length: 6,
+            max_length: 64
+        },
+        password: {
+            type: String,
+            required: true,
+            min_length: 8,
+            max_length: 64
+        },
+        friends: [friendSchema],
+        thoughts: [thoughtSchema],
+        likes: [likeSchema],
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
