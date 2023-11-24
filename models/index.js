ad32b1cb7a18f38ff06e1dfce2580026c85f9616
Ryan England <me@rengland.org>
2023-11-24 15:06:33 -0800

    Added models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Friend = require('./Friend');
+const Like = require('./Like');
+const Thought = require("./Thought");
+const User = require("./User");
+
+module.exports = { Friend, Like, Thought, User };
